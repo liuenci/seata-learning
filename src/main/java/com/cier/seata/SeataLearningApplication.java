ce895f884cab02db6bb101a2349fc152ee7f741c
liuenci <liuenci@cvte.com>
2021-02-20 08:58:51 +0800

    [seata-learning] seata 多数据源demo完整版

---
+++ src/main/java/com/cier/seata/SeataLearningApplication.java
@@ -0,0 +1,13 @@
+package com.cier.seata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeataLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeataLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cier/seata/SeataLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cier.seata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeataLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
